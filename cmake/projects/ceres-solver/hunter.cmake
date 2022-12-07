9d6ce72c3b73f612b2dffdb206bb6d8b5c7779a7
Reinhold Gschweicher <pyro4hell@gmail.com>
2022-12-07 16:46:25 +0100

    ceres-solver: update to v2.1.0-p0 with OpenBLAS support
    Update `ceres-solver` to `v2.1.0-p0`.
    
    Add option `WITH_OPENBLAS` (disabled in Hunter per default) to build
    against `OpenBLAS` instead of generic `LAPACK`.
    
    `OpenBLAS` `v0.3.21` provides a f2c-converted `LAPACK` `v3.9.0` copy,
    which is used when building without a Fortran compiler.
    This enables us to have a C++ only build, making static libraries
    easier (no fortran runtime libs anymore).

--- cmake/projects/ceres-solver/hunter.cmake
+++ cmake/projects/ceres-solver/hunter.cmake
@@ -76,6 +76,17 @@ hunter_add_version(
     868f7b45b127d4eb2777ce889e0d273bd11f5f73
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    ceres-solver
+    VERSION
+    "2.1.0-p0"
+    URL
+    "https://github.com/cpp-pm/ceres-solver/archive/refs/tags/v2.1.0-p0.tar.gz"
+    SHA1
+    9c1bcb08100acb95fac0ea3e5ffdf4990ae904d3
+)
+
 hunter_cmake_args(ceres-solver CMAKE_ARGS
     # explicitly remove dependency on gflags (only needed for tests)
     GFLAGS=OFF
@@ -83,6 +94,8 @@ hunter_cmake_args(ceres-solver CMAKE_ARGS
     LAPACK=OFF
     SUITESPARSE=OFF
     CXSPARSE=OFF # since 1.14.0-p0
+    # OpenBLAS flag, alternative to LAPACK since v2.1.0-p0
+    WITH_OPENBLAS=OFF
     # don't build tests
     BUILD_TESTING=OFF
     # also don't build examples: when suitesparse is enabled the examples need Fortran libraries
